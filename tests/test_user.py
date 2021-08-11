4385e37075fa8902b409a757fc1e9a3fe56a96a6
swallahfx <adeniyisaheed75@gmail.com>
2021-08-11 14:56:26 +0100

    for test_all method

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,14 +1,28 @@
 import unittest
-
-class TestUser:
-    pass
-
+from models.user import User
+from models.connection import Connect
 
 
 
+class BookTest(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = Connect.conn_psycopg2()
+        self.cursor= self.conn.cursor()
+        self.book = User()
+
+    def test_all(self):
+        result = self.book.all()
+        self.assertIsInstance(result, list)
+        self.assertNotIsInstance(result, str)
+        self.assertIsNotNone(result)
 
 
+    def tearDown(self):
+        self.conn = None
+        self.cursor= None
+        self.book = None
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
