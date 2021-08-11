aa1ecce5ee3e4cfc407e9a9ed4ab33186ab9cf0f
swallahfx <adeniyisaheed75@gmail.com>
2021-08-11 15:16:29 +0100

    user.(all)_logic

--- models/user.py
+++ models/user.py
@@ -1,2 +1,12 @@
-class User:
-    pass
+from connection import Connect
+from datetime import datetime
+
+
+class User(Connect):
+    def __init__(self):
+        self.connection = Connect().conn_ps()
+        self.cursor = self.connection.cursor()
+    
+    def all(self):
+        self.cursor.execute("SELECT * FROM users")
+        return self.cursor.fetchall()
